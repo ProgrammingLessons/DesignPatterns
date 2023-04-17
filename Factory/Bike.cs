b9f1211038292dfc350bf8e104a90cee13c590b5
eiscan <eiscan@outlook.com.tr>
2023-04-17 12:37:23 +0300

    renewed

---
+++ Factory/Bike.cs
@@ -0,0 +1,23 @@
+﻿namespace Factory
+{
+    // The bike class inherits from the IVehicle interface.
+    public class Bike : IVehicle
+    {
+        private readonly int Wheels;
+
+        public Bike()
+        {
+            Wheels = 2;
+        }
+        // The internals of the implemented methods are written.
+        public int NumberOfWheels()
+        {
+            return Wheels;
+        }
+
+        public string VehicleType()
+        {
+            return "Bike class";
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // We specify which class we want to create from the VehicleFactory class to the GetVehicle method.
+            var bike = VehicleFactory.GetVehicle("bike");
+            var car = VehicleFactory.GetVehicle("car");
+            var rickshaw = VehicleFactory.GetVehicle("rickshaw");
+            var bus = VehicleFactory.GetVehicle("bus");
+        }
+    }
+}
