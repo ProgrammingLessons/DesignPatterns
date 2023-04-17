ef30365a0e539e7a082c206825844e98eaa9593b
eiscan <eiscan@outlook.com.tr>
2023-04-17 12:22:26 +0300

    ​factory design pattern done

---
+++ FactoryExam1/IVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryExam1
+{
+    // Interface definition is made for the vehicle.
+    public interface IVehicle
+    {
+        // Two methods will be used in this interface
+        string VehicleType();
+        int NumberOfWheels();
+    }
+}
